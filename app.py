35ada0533377cba890d5d95e488494db533fdc09
theophilus <theophilus.ogbolu@gmail.com>
2021-01-05 14:41:42 +0100

    second

--- app.py
+++ app.py
@@ -1,33 +1,97 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 driver = webdriver.Chrome()
+# driver.implicitly_wait(60*60)
 driver.get("https://web.whatsapp.com/")
 assert "WhatsApp" in driver.title
 
 
-print("\n"*10)
-input("Scan QR and press any key to console")
-
-person = driver.find_element_by_xpath("span.VOr2j")
-person.click()
-
-
-
-inputField = driver.find_element_by_xpath("//div[text()='Type a message']/..")
-# inputfield = driver.find_element_by_xpath("//*[text()='Type a message']/../div[@class='_1awRl']")
-inputfield.click()
-inputfield.
-# inputfield.clear()
-inputfield.send_keys("pycon", Keys.RETURN)
-
-# assert "No results found." not in driver.page_source
+def decorate():
+    print("\n"*10)
+    print("*"*50)
+    # input("Scan QR 10s")
+    print("Please Scan QR to continue")
+    print("*"*50)
+    print("\n"*2)
 
 
-chatArea = driver.find_element_by_xpath("//div[@aria-label='Message list. Press right arrow key on a message to open message context menu.']")
-received = chatArea.find_element_by_class_name('message-in')
+# driver.implicitly_wait(0)
+decorate()
+
+botContact = ["None", "Theophilus"]
+botContact = [x.lower() for x in botContact]
+botActive = True
 
 
+def Msg():
+    lastMsgIn = driver.find_element_by_xpath('//div[contains(@class, "_1RAno message-in")][last()]//span[@dir="ltr"]/span')
+    lastMsg = driver.find_element_by_xpath('//div[contains(@class, "_1RAno message")][last()]//span[@dir="ltr"]/span')
+
+    if lastMsg == lastMsgIn:
+        inputField = driver.find_element_by_xpath("//footer//div[contains(@class, 'copyable-text')]")
+        inputField.clear()
+
+        command = lastMsgIn.text.lower().split(" ")
+        if command[0] == "aris,,":
+
+            if command[1] == "add":
+                botContact.append(command[2])
+                inputField.send_keys("done", Keys.RETURN)
+
+            elif command[1] == "remove":
+                print("remove")
+                botContact.remove(command[2])
+                inputField.send_keys("done", Keys.RETURN)
+
+            elif command[1] == "list":
+                inputField.send_keys("\n".join(botContact), Keys.RETURN)
+
+            elif command[1] == "pause":
+                print("pause")
+                botActive = False
+                inputField.send_keys("done", Keys.RETURN)
+
+            elif command[1] == "start":
+                print("start")
+                botActive = True
+                inputField.send_keys("done", Keys.RETURN)
+
+            elif command[1] == "stop":
+                inputField.send_keys("done", Keys.RETURN)
+                driver.close()
+            
+            else:
+                inputField.send_keys("unknown command", Keys.RETURN)
+
+        else:
+            inputField.send_keys(f"You typed: {lastMsgIn.text}", Keys.RETURN)
 
 
-# driver.close()
+def BotAction():
+    try:
+        unreadContact = driver.find_elements_by_xpath('//span[contains(@class, "VOr2j")]/ancestor::div[contains(@class, "_1MZWu")]')
+        unreadContact = unreadContact[0]
+        unreadContactName = unreadContact.text.split("\n")[0]
+
+        if unreadContactName.lower() in botContact:
+            unreadContact.click()
+            Msg()
+    except:
+        pass
+    # except Exception as ex:
+        # print(f"{ex}- no new message on contact list")
+
+    try:
+        activeContactName = driver.find_element_by_xpath('//header[@class="_1UuMR"]//span[contains(@dir, "auto")][contains(@title, "")]')
+        if activeContactName.text.lower() in botContact:
+            Msg()
+    except:
+        pass
+
+
+while True:
+    if botActive:
+        BotAction()
+    time.sleep(1)
--- speak.py
+++ speak.py
@@ -2,9 +2,14 @@ import pyttsx3
 
 tts = pyttsx3.init()
 
-rate = tts.setProperty("rate", 150)
+tts.setProperty("rate", 150)
 tts.setProperty('voice', tts.getProperty('voices')[1].id)
 
-tts.say('The quick brown fox jumped over the lazy dog.')
-tts.runAndWait()
-tts.stop()
+
+def Say(msg):
+    tts.say(msg)
+    tts.runAndWait()
+    tts.stop()
+
+
+Say("hello")
